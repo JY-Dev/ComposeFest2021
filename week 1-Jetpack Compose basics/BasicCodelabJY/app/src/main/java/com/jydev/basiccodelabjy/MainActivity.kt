1dc479de0b200c8d8494d99051a29c1fa421d845
JY-Dev <kjy0302014@gmail.com>
2021-11-01 23:14:11 +0900

    Init

---
+++ week 1-Jetpack Compose basics/BasicCodelabJY/app/src/main/java/com/jydev/basiccodelabjy/MainActivity.kt
@@ -0,0 +1,42 @@
+package com.jydev.basiccodelabjy
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.jydev.basiccodelabjy.ui.theme.BasicCodelabJYTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            BasicCodelabJYTheme {
+                Greeting(name = "Jaeyoung")
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Surface(color = Color.Gray) {
+        Text(text = "Hello $name!", modifier = Modifier.padding(16.dp), color = Color.White)
+    }
+    
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    BasicCodelabJYTheme {
+        Greeting("Android")
+    }
+}
